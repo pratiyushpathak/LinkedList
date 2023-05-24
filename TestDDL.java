afbbecb063b171aae8b903cb5d22f566596992df
Pratiyush Pathak <56939096+pratiyushpathak@users.noreply.github.com>
2023-05-25 03:34:24 +0530

    Add files via upload

---
+++ TestDDL.java
@@ -0,0 +1,10 @@
+public class TestDDL {
+
+        public static void main(String[] args) {
+            DLinkedList list=new DLinkedList();
+            list.insertAtBeg(5);
+            list.insertAtBeg(6);
+            list.insertAtLast(8);
+            list.forwordDisplay();
+        }
+    }
